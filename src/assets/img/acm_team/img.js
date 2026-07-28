7ce8a9c25c9d8a1f312199eea4e7c2c799c135d9
Gaurangi <gaurangigg12@gmail.com>
2026-07-28 18:16:45 +0530

    changes

--- src/assets/img/acm_team/img.js
+++ src/assets/img/acm_team/img.js
@@ -13,6 +13,14 @@ import milan_singhal from "./boe/milan_singhal.webp";
 import rishitt_gupta from "./boe/rishitt_gupta.webp";
 import smriti_walia from "./boe/smriti_walia.webp";
 import supragya_gandotra from "./boe/supragya_gandotra.webp";
+// new boe images
+import sangam from "./boe/added/Sangam_Khanna.jpg";
+import Tanay_Prabhakar from "./boe/added/Tanay_Prabhakar.png";
+import Rudransh from "./boe/added/Rudransh.jpg";
+import Advika_Kaushik from "./boe/added/Advika_Kaushik.jpg";
+import Daksh_Mehrotra from "./boe/added/Daksh_Mehrotra.JPG";
+import rahul_mankani from "./boe/added/rahul_mankani.jpg";
+import Ahad_Khan from "./boe/added/Ahad_Khan.jpg";
 // executives images
 import devanshi_raghav from "./executives/devanshi_raghav.jpg";
 import atharv_chandorikar from "./executives/atharv_chandorikar.webp";
@@ -20,6 +28,11 @@ import kinjal_srivastava from "./executives/kinjal_srivastava.webp";
 import stuti_jain from "./executives/stuti_jain.webp";
 import hritvik_garg from "./executives/hritvik_garg.jpg";
 import sachi_choudhary from "./executives/sachi_choudhary.webp";
+//new executive/s images
+import Niharika_Chand from "./executives/added/Niharika_Chand.jpg";
+import Mishthi_Nagpal from "./executives/added/Mishthi_Nagpal.jpg";
+import Devaansh_Kushwaha from "./executives/added/Devaansh_Kushwaha.jpg";
+import Jiya_Singh from "./executives/added/Jiya_Singh.jpg";
 
 // office bearers images
 import aanchal_agarwal from "./office_bearers/aanchal_agarwal.webp";
@@ -34,6 +47,23 @@ import raghav_singhal from "./office_bearers/raghav_singhal.jpg";
 import sudhanshu_ranjan from "./office_bearers/sudhanshu_ranjan.jpg";
 import vridhi_jain from "./office_bearers/vridhi_jain.jpeg";
 import vrishali_sahay from "./office_bearers/vrishali_sahay.webp";
+// added office bearers images
+import gg from "./office_bearers/added/gg.png";
+import Amaan_Rahman from "./office_bearers/added/Amaan_Rahman.jpeg";
+import Akshita_Gupta from "./office_bearers/added/Akshita_Gupta.jpeg";
+import Sonavdeep_Sharma from "./office_bearers/added/Sonavdeep_Sharma.jpg";
+import Prarthana_Baisoya from "./office_bearers/added/Prarthana_Baisoya.jpg";
+import Vibhuti_Negi from "./office_bearers/added/Vibhuti_Negi.jpg";
+import Sanvi_Mukherjee from "./office_bearers/added/Sanvi_Mukherjee.jpg";
+import Ved_Mahajan from "./office_bearers/added/Ved_Mahajan.jpg";
+import Priyanshi_Rana from "./office_bearers/added/Priyanshi_Rana.png";
+import Ruhani_Khurana from "./office_bearers/added/Ruhani_Khurana.jpeg";
+import Ronit_Batra from "./office_bearers/added/Ronit_Batra.jpeg";
+import Sakshi_Kumari from "./office_bearers/added/Sakshi_Kumari.png";
+import Shrimann_Bansal from "./office_bearers/added/Shrimann_Bansal.JPG";
+import Aastha_Dhavan from "./office_bearers/added/Aastha_Dhavan.jpeg";
+import Madhav_Tiwari from "./office_bearers/added/Madhav_Tiwari.jpg";
+import Grikzhana_Daulagupu from "./office_bearers/added/Grikzhana_Daulagupu.jpg";
 
 const img = {
     aanshi,
@@ -67,7 +97,35 @@ const img = {
     raghav_singhal,
     sudhanshu_ranjan,
     vridhi_jain,
-    vrishali_sahay
+    vrishali_sahay,
+    //added
+    sangam,
+    Tanay_Prabhakar,
+    Rudransh,
+    Advika_Kaushik,
+    Daksh_Mehrotra,
+    rahul_mankani,
+    Ahad_Khan,
+    Niharika_Chand,
+    Mishthi_Nagpal,
+    Devaansh_Kushwaha,
+    Jiya_Singh,
+    gg,
+    Amaan_Rahman,
+    Akshita_Gupta,
+    Prarthana_Baisoya,
+    Sonavdeep_Sharma,
+    Vibhuti_Negi,
+    Sanvi_Mukherjee,
+    Ved_Mahajan,
+    Priyanshi_Rana,
+    Ruhani_Khurana,
+    Ronit_Batra,
+    Sakshi_Kumari,
+    Shrimann_Bansal,
+    Aastha_Dhavan,
+    Madhav_Tiwari,
+    Grikzhana_Daulagupu
 };
 
 export default img;
